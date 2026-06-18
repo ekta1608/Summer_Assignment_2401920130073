a6a5ee2c51c6f38c07e127babdd9bc17c3a43cd5
ekta1608 <singhektaa206@gmail.com>
2026-06-18 21:19:54 +0530

    Implement largest rectangle in histogram algorithm

---
+++ Week_3/Day 4/largest_rec_in_histogram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() &&
+                  (i == n || heights[st.top()] >= heights[i])) {
+
+                int height = heights[st.top()];
+                st.pop();
+
+                int left = st.empty() ? -1 : st.top();
+                int width = i - left - 1;
+
+                maxArea = max(maxArea, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};
